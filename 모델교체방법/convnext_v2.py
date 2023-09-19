8d7d6b2c68bdfff9875fbc2848f7d8828305a444
Semmn <ke9429395@gmail.com>
2023-09-19 17:28:17 +0900

    modify model change files

--- 모델교체방법/convnext_v2.py
+++ 모델교체방법/convnext_v2.py
@@ -4,12 +4,13 @@ import timm
 class ConvNextV2(nn.Module):
     def __init__(self, n_outputs:int, **kwargs):
         super(ConvNextV2, self).__init__()
-        self.model = timm.create_model('convnextv2_base', pretrained=True)
+        self.model = timm.create_model('convnextv2_nano', pretrained=True)
         self.model.classifier = nn.Sequential(
-            nn.Linear(in_features=1792, out_features=625),
+            nn.Linear(in_features=1024, out_features=512),
             nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(in_features=625, out_features=256),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=512, out_features=256),
+            nn.BatchNorm1d(num_features=256),
             nn.ReLU(),
             nn.Linear(in_features=256, out_features=n_outputs)
         )
